8ce9684ac0e2f130acfae4433bd31f0b8b9ab04d
Hoptilic <hoptilic@yahoo.com>
2020-07-31 12:32:11 +0200

    Update guiHandler

--- src/main/java/ml/knightcraft/core/utils/guiHandler.java
+++ src/main/java/ml/knightcraft/core/utils/guiHandler.java
@@ -1,4 +1,23 @@
 package ml.knightcraft.core.utils;
 
-public class guiHandler {
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class guiHandler implements Listener {
+    @EventHandler
+    public void onMenuClick(InventoryClickEvent e){
+        e.setCancelled(true);
+
+        Player p = (Player) e.getWhoClicked();
+        if(e.getView().getTitle().equalsIgnoreCase("Select a function")){
+            if(e.getCurrentItem().getType().equals(Material.RED_WOOL)) {
+                p.sendMessage(ChatColor.RED + "It works!");
+                p.closeInventory();
+            }
+        }
+    }
 }
